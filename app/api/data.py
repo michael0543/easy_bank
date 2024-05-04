d50d457bfe4288e4eccdd3b44eb2c6b5f594e462
Michael JF Tsai <michael0543@yahoo.com.tw>
2024-05-04 20:04:37 +0800

    [feature] add app/api/data.py for output data api router

---
+++ app/api/data.py
@@ -0,0 +1,26 @@
+from fastapi import APIRouter, Depends
+from fastapi.responses import StreamingResponse
+
+from app.database.connection import DataBase, get_database
+
+router = APIRouter()
+
+@router.get("/data/account")
+def output_account_data(db: DataBase = Depends(get_database)):
+    data = db.output_account_csv()
+    response = StreamingResponse(
+        data,
+        media_type="text/csv",
+        headers={"Content-Disposition": f"attachment; filename=account.csv"}
+    )
+    return response
+
+@router.get("/data/transaction")
+def output_transaction_data(db: DataBase = Depends(get_database)):
+    data = db.output_transaction_csv()
+    response = StreamingResponse(
+        data,
+        media_type="text/csv",
+        headers={"Content-Disposition": f"attachment; filename=transaction.csv"}
+    )
+    return response
