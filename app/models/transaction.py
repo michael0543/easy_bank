d89e027db643bddef13e419b8408b069298a8892
Michael JF Tsai <michael0543@yahoo.com.tw>
2024-05-04 20:01:45 +0800

    [feature] add app/models/transaction.py for fastapi models

---
+++ app/models/transaction.py
@@ -0,0 +1,31 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+class TransactionBase(BaseModel):
+    amount: int
+    timestamp: datetime
+
+class TransactionCreate(BaseModel):
+    account_id: int
+    type: str
+    amount: int
+
+class Transaction(BaseModel):
+    transaction_id: int
+    account_id: int
+    type: str
+    amount: str
+    timestamp: datetime
+
+class DepositCreate(BaseModel):
+    name: str
+    amount: int
+
+class WithdrawCreate(BaseModel):
+    name: str
+    amount: int
+
+class TransferCreate(BaseModel):
+    sender_name: str
+    receiver_name: str
+    amount: int
