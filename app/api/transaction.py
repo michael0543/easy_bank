19e4d0f019177c1285c46140d039bdf3f610e044
Michael JF Tsai <michael0543@yahoo.com.tw>
2024-05-04 20:03:52 +0800

    [feature] add app/api/transaction.py for transaction related api router

---
+++ app/api/transaction.py
@@ -0,0 +1,24 @@
+from fastapi import APIRouter, HTTPException, Depends
+
+from app.models.transaction import DepositCreate, WithdrawCreate, TransferCreate
+from app.database.connection import DataBase, get_database
+
+router = APIRouter()
+
+@router.post("/transaction/deposit")
+def deposit(deposit_data: DepositCreate, db: DataBase = Depends(get_database)):
+    db.deposit(deposit_data)
+    return {"message": "Deposit successfully"}
+
+@router.post("/transaction/withdraw")
+def withdraw(withdraw_data: WithdrawCreate, db: DataBase = Depends(get_database)):
+    try:
+        db.withdraw(withdraw_data)
+        return {"message": "Withdraw successfully"}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@router.post("/transaction/transfer")
+def transfer(transfer_data: TransferCreate, db: DataBase = Depends(get_database)):
+    db.transfer(transfer_data)
+    return {"message": "Transfer successfully"}
