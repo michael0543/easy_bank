957dfc65badb7879b234c91edacdc83bbc199681
Michael JF Tsai <michael0543@yahoo.com.tw>
2024-05-04 20:01:20 +0800

    [feature] add app/models/account.py for fastapi models

---
+++ app/models/account.py
@@ -0,0 +1,19 @@
+from typing import Optional
+from pydantic import BaseModel
+
+
+class Account(BaseModel):
+    account_id: int
+    name: str
+    balance: int
+
+class AccountCreate(BaseModel):
+    name: str
+    balance: Optional[int] = 0
+
+class AccountUpdate(BaseModel):
+    name: str
+    balance: int
+
+class AccountName(BaseModel):
+    name: str
