e257d4541ad311abe932b4bf8f746104eb69ab2c
Michael JF Tsai <michael0543@yahoo.com.tw>
2024-05-06 08:43:51 +0800

    [fix] fix typo

--- app/schema/transaction.sql
+++ app/schema/transaction.sql
@@ -1,6 +1,6 @@
 CREATE TABLE IF NOT EXISTS transaction (
     transation_id SERIAL,
-    accout_id CHARACTER VARYING,
+    account_id CHARACTER VARYING,
     type CHARACTER VARYING,
     amount INT,
     timestamp TIMESTAMP WITHOUT TIME ZONE
